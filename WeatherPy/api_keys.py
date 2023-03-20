294464701e49e02f9ff6921f833a6915b1f8c9d4
Marcela Sierzega <mwsierzega@gmail.com>
2023-03-19 21:48:17 -0700

    added geoapify api key

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "8c275ff7660be16800f61f05d41a47a2"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "fce62601c6fb44b8be7e2b3d9efe275e"
